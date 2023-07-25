e70407d528bef6910588d1fad99fa70975a75330
Talha <talhashahidch@gmail.com>
2023-07-26 00:42:50 +0500

    Individual Listing View @ 6 : 16

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -7,6 +7,13 @@ export default async function getListings() {
         createdAt: "desc",
       },
     });
-    return listings;
-  } catch (error: any) {}
+
+    const safeListings = listings.map((listing) => ({
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+    }));
+    return safeListings;
+  } catch (error: any) {
+    throw new Error(error);
+  }
 }
